cdf562c42db906dc103b917d7349d8da1fe1b34f
Ashutosh Yadav <ashutosh2213072@akgec.ac.in>
2025-11-17 15:48:46 +0530

    url

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const API_BASE =
   import.meta.env.VITE_API_BASE_URL ||
-  "https://audience-query-management-response-system.onrender.com";
+  "https://aqmrs-backend.onrender.com";
 
 export const TOKEN_KEY = "aqmrs_token";
