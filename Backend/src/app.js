0e54ebdcc4f9ed303588d112b090a73b25340655
Ashutosh Yadav <ashutosh2213072@akgec.ac.in>
2025-12-16 17:29:49 +0530

    fix issue

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -41,7 +41,8 @@ app.use(
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://audience-query-management-response-theta.vercel.app"
+  "https://audience-query-management-response-theta.vercel.app",
+   "https://www.audiencequery.online",
 ];
 
 app.use(
